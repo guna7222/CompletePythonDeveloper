b1db9a565711252a4ebb6e165cc1f046430f1603
guna7222 <rrgunasekhar@gmail.com>
2021-08-12 07:18:46 +0530

    Complete Python Developer

---
+++ Python_Interduction/programming_language.py
@@ -0,0 +1,34 @@
+"""
+What is a programming language:-
+Programming language is the simple way to give instructions to the computers,
+we will give it as manual instructions and then it follows.
+
+Without programming computer programs wouldn't do anything, you mouse courser  wouldn't work, you may might not
+google a things and etc.
+"""
+
+var = ("\n"
+       "Computer's don't understand Human's language they understand only binary numbers 0's and 1's\n"
+       "all electronics speaks that language on and off\n")
+print(var)
+
+
+def lower_higher(low_level, high_level):
+    result = low_level + high_level
+    print(result)
+
+
+low_level = 'lower level languages are close to machine language \n'
+high_level = 'higher level languages are closer to english languages'
+
+lower_higher(low_level, high_level)
+
+# Python would come in which level lower or higher?
+answer = 'Python would come under high level language'
+
+# Interpreter
+interpreter = 'Interpreter translates line by line and executes the line before going to another line'
+
+# Compiler
+compiler = "Compiler translates all lines of a programs into binary file and executes the whole file at ones"
+
