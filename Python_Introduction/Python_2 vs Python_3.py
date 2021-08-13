65416f46dfb479cab2f35d1779afc3a56d170fb6
guna7222 <rrgunasekhar@gmail.com>
2021-08-13 11:06:34 +0530

    Python_2 vs Python_3

---
+++ Python_Introduction/Python_2 vs Python_3.py
@@ -0,0 +1,8 @@
+# Python 2.0
+# print 'hello world'
+
+# Python 3.0
+print('Hello World')
+
+#  Python 2.0 is Legacy it means they stopped updating security patches.
+# The Python latest version is 3.9
