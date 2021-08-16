894cdd52d394d2bbafd9e59731d7deee1279fdbd
guna7222 <rrgunasekhar@gmail.com>
2021-08-16 07:55:42 +0530

    Data types

---
+++ PythonBasics/Learn_a_Language.py
@@ -0,0 +1,8 @@
+"""
+To learn a Language for any programmer there are four key terms
+i) Terms
+ii) Data Types
+iii) Actions
+iv) Best practices
+"""
+# At the end of the day all programs is the way to tell computer store, retrieve and perform some action on it.
