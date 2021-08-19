906ec5694f5b4e6d91abb7cb0a12dc6fa53eefc4
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-08-19 11:41:45 +0530

    Added strings

---
+++ PythonBasics/Strings.py
@@ -0,0 +1,18 @@
+# String
+"""
+A string is a sequence of character's or a string is a piece of text
+"""
+
+print(type('Gunasekhar'))
+
+user_name = 'gunasekhar'
+passwd = '89gu12kl'
+long_string = """
+WOW
+o o
+---
+"""
+
+first_name = 'gunasekhar '
+last_name = 'Ragavaraju'
+print(first_name + last_name)
