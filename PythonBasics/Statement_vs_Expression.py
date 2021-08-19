a0abb8048cfc6b76b985a33b04e0a01e6d547aef
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-08-19 11:00:57 +0530

    Added Statements vs Expressions

---
+++ PythonBasics/Statement_vs_Expression.py
@@ -0,0 +1,12 @@
+"""
+Expression :- is a piece of code that produces a values.
+
+Statement:- Entire code of line code that performs some sort of action
+
+Expression( Expression is a combination of variables, values and operators that yields to a result value)
+Statement( A statement presents an action or command eg:- print statement, assignment statement)
+
+"""
+a = 10
+b = 2/10
+print(b)
