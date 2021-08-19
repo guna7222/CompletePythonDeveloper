0869e442521035d1645b6039aec792e864242d0e
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-08-19 11:20:26 +0530

    Augmented Assignment Operator

---
+++ PythonBasics/Augmented_Assignment_Operator.py
@@ -0,0 +1,17 @@
+# Augmented Assignment Operator is called short-hand
+programming_language = 'Python'
+greetings = programming_language + 'is a high-level'
+print(greetings)
+
+# Augmented assignment operator
+programming_language = 'Python'
+programming_language += 'is used in different domains'
+print(programming_language)
+
+num = 22
+num += 23
+print(num)
+
+sub = 22
+sub -= 2
+print(sub)
