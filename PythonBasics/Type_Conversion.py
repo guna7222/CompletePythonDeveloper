c86ca5b1d6b0b93e8084b4d7c32503416da5bc06
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-08-19 12:23:14 +0530

    Added Type Conversion

---
+++ PythonBasics/Type_Conversion.py
@@ -0,0 +1,13 @@
+"""
+Type Conversion:- Type conversion refers to the conversion of one data type into another
+data type is called Type Conversion
+"""
+a = str(100)
+b = int(a)
+c = type(b)
+print(c)
+
+# same as above one
+
+print(type(int(str(100))))
+
