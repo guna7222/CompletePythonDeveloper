9375744b81e85b77ed329e572a88362638b228cd
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-08-19 17:59:12 +0530

    Added Escape Sequence

---
+++ PythonBasics/Escape_Sequence.py
@@ -0,0 +1,10 @@
+# Escape Sequence
+text = 'it\'s a kind of human thing'
+text_1 = "it\'s a kind of human thing"
+
+new_line = "Python is a general purpose \nhigh-level interpreted programming language"
+print(new_line)
+
+tab_space = "\t it\\s a kind of human thing"
+print(tab_space)
+
