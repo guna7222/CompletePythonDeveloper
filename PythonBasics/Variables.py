a1f0413b6a471d5e2381c2f44c5264c9a27abe81
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-08-19 11:01:53 +0530

    Add files via upload
    Added Variables

---
+++ PythonBasics/Variables.py
@@ -0,0 +1,42 @@
+# Variables
+"""
+Programs are simply data being stored, being changed, and removed that's all programs
+all.
+
+Assigning a value to the variable is also called binding.
+"""
+
+# Variables are containers for storing data types.
+
+"""
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+
+"""
+
+a = 10
+b = 20
+print(a+b)
+
+# Constants
+PI = 3.14
+
+# Many values to Multiple Variables
+a, b, c = 1, 2, 3
+print(a)
+print(b)
+print(c)
+
+# One value to Multiple variables
+
+a = b = c = 12
+print(a)
+print(b)
+print(c)
+
+# Casting in Python
+# Casting is used to specify the data type of the object
+name = str('gunasekhar')
+print(name)
