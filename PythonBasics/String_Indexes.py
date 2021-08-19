985ded2495e7e2b9f52440408438ef2a7a01bb0e
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-08-19 22:17:57 +0530

    Added String Indexes

---
+++ PythonBasics/String_Indexes.py
@@ -0,0 +1,20 @@
+# String Indexes
+name = 'gunasekhar'
+
+# In python index start from 0
+print(name[0])
+
+# [start : stop : step over]
+print(name[0:5:1])
+
+print(name[1:])
+
+print(name[:5])
+
+print(name[::1])
+
+print(name[::-1])
+
+print(name[-1])  # starts from the end
+
+
