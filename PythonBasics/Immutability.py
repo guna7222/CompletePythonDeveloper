461c58a7236f2fbcabeb5ef86a4c2199e98ab42d
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-08-20 10:33:38 +0530

    Added Immutability

---
+++ PythonBasics/Immutability.py
@@ -0,0 +1,15 @@
+"""
+Strings are immutable:-
+
+programming = 'Python'
+programming[0] = 'J'
+print(programming)
+
+"""
+# You can't reassign a specific part of a string
+
+# Slicing 
+"""
+you can return a range of character's in a string by using slicing, specify
+the start and end index, separated by colon, to return a part of string. 
+"""
