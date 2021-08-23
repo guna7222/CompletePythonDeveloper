c496e8b23993781db1cb9f5adf473f74f1b23dd4
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-08-23 19:34:22 +0530

    Added password checker

---
+++ PythonBasics/password_checker.py
@@ -0,0 +1,5 @@
+user_name = 'gunasekhar'
+password = 'Python@7222'
+length_of_password = len(password)
+
+print(f'{user_name}, your password is {"*" * length_of_password} and length is {length_of_password}')
