6cf4a19f9fb505feeda9db6734a860ee1c74c442
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-09-03 18:08:25 +0530

    Added tuples

---
+++ PythonBasics/tuple.py
@@ -0,0 +1,15 @@
+# Tuple
+# A tuple is a collection which is ordered, unchangeable, and allow duplicate items.
+weapons = ('hello', 'world', 'python')
+print(weapons[1:2])
+
+print('python' in weapons)
+
+print(len(weapons))
+
+# tuple methods
+# count
+print(weapons.count('python'))
+
+# index
+print(weapons.index('world'))
