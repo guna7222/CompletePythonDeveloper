2645e925d965676aa82d4f151a00d37752b64e78
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-09-06 15:44:45 +0530

    Update indentation.py

--- Python_Basics_2/indentation.py
+++ Python_Basics_2/indentation.py
@@ -1,1 +1,13 @@
 # Indentation 
+"""
+Indentation refers to the white spaces at the beginning of the code where in other programming languages 
+indentation is used to code readability but in python indentation refers a block of code
+"""
+# Actually indentation spaces is upto as a programmer but it actually done by using four spaces or a tab.
+
+is_he_won_car = True
+
+if he_won_car:
+  print('Yes')
+else:
+  print('No')
