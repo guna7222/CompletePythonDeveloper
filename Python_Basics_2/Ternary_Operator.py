2c71f40f553323fe122e51a4b677f83146d8e999
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-09-07 08:45:11 +0530

    Added Ternary Operator

---
+++ Python_Basics_2/Ternary_Operator.py
@@ -0,0 +1,9 @@
+# Ternary Operator
+# if_condition_True if condition else if_condition_False
+
+is_friend = True
+message = "allowed to send msg" if is_friend else "not allowed"
+print(message)
+
+a, b = 10, 20
+print("a is greater than b" if a>b else "a is less than b")
