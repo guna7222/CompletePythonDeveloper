75918d86157d029e22ee92d6c4a3896e652bc5d9
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-09-13 23:03:39 +0530

    Added first gui

---
+++ Python_Basics_2/first_gui.py
@@ -0,0 +1,16 @@
+# Our first GUI
+pictures = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0]
+]
+for i in pictures:
+    for j in i:
+        if j == 0:
+            print(' ', end='')
+        else:
+            print('*', end='')
+    print('')
