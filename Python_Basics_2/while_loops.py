49156d48d0f429d5000154dd5a981932790da681
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-09-13 16:21:36 +0530

    Added while loops

---
+++ Python_Basics_2/while_loops.py
@@ -0,0 +1,12 @@
+# with while loop we can execute a set of statements as long as condition is True
+i = 0
+while i < 50:
+    print(i)
+    i = i+1
+
+i = 1
+while i<5:
+    print(i)
+    i = i+1
+    break
+
