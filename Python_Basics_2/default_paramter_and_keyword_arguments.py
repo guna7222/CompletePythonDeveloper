361bc51b45fed785f6c33dc7c4a8bb864deb0e54
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-09-14 21:58:36 +0530

    Added default parameters

---
+++ Python_Basics_2/default_paramter_and_keyword_arguments.py
@@ -0,0 +1,15 @@
+# Default parameters and keyword arguments
+# positional arguments
+# keyword arguments
+# default parameters
+def audi(model='audi-8', color ='pure black', price='2.4 cr'):
+    print(f'hello model {model} price {price} color {color}')
+
+
+# positional arguments
+audi(19121, 'x', 'white')
+# keyword arguments
+audi(model='x', color='white', price=12132)
+
+# default parameters
+audi()
