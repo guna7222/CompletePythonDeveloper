1e1782cc183384b9f10942438877bf81c16e7c14
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-09-14 19:59:47 +0530

    Added arguments and parameters

---
+++ Python_Basics_2/argument_vs_parameter.py
@@ -0,0 +1,14 @@
+# Argument vs Parameter
+"""
+A parameter is the variable listed inside the parentheses in the function definition.
+
+An argument is the value that are sent to the function when it is called.
+"""
+def userDetails(name, age): # name, age are examples for parameters
+    print(name)
+    print(age)
+
+
+userDetails('gunasekhar', 23) # this is example for arguments
+# arguments is also called as call, invoke
+
