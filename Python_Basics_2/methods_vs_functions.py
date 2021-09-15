2749b43f394ea578c4cd580312d25d3ec5e8135f
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-09-15 13:03:07 +0530

    Added methods and functions

---
+++ Python_Basics_2/methods_vs_functions.py
@@ -0,0 +1,12 @@
+# method vs functions
+# a method is a function that belongs to an object
+'my world'.capitalize()  # method example
+
+
+# functions
+# a function is a block of code which will execute when a function is called
+def add(a, b):
+    return a + b
+
+
+print(add(12, 12))
